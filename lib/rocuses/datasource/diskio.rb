227a65f15616da10f48de60106cfac2a2922671a
Toshifumi Sakaguchi <siskrn@gmail.com>
2013-02-17 23:56:52 +0900

    Rocuses::DataSource::DiskIO::update内にて、RRDファイルを作成するタイミングを変更

--- lib/rocuses/datasource/diskio.rb
+++ lib/rocuses/datasource/diskio.rb
@@ -34,14 +34,14 @@ module Rocuses
       end
 
       def update( config, resource )
-        @read_data_size   = create_rrd( config, 'read_data_size' )
-        @read_count       = create_rrd( config, 'read_count' )
-        @write_data_size  = create_rrd( config, 'write_data_size' )
-        @write_count      = create_rrd( config, 'write_count' )
-
         if resource.disk_ios
           resource.disk_ios.each { |disk_io|
             if disk_io.name == @name
+              @read_data_size   = create_rrd( config, 'read_data_size' )
+              @read_count       = create_rrd( config, 'read_count' )
+              @write_data_size  = create_rrd( config, 'write_data_size' )
+              @write_count      = create_rrd( config, 'write_count' )
+
               @read_data_size.update(  disk_io.time, disk_io.read_data_size )
               @read_count.update(      disk_io.time, disk_io.read_count )
               @write_data_size.update( disk_io.time, disk_io.write_data_size )
