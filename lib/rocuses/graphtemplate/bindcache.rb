02c22524c1d88d9e09ebd87d61e1d49785b8a6ba
Toshifumi Sakaguchi <sischkg@gmail.com>
2013-05-26 23:07:44 +0900

    BIND cacheのグラフにて、レコードごとに線のスタイルを設定した。

--- lib/rocuses/graphtemplate/bindcache.rb
+++ lib/rocuses/graphtemplate/bindcache.rb
@@ -13,6 +13,37 @@ module Rocuses
 
       GPRINT_FORMAT = '%7.0lf'
 
+      LINE_STYLE_OF = {
+        'A'        => { :color => '#ff0000', :daches => false, :priority => 100, },
+        '!A'       => { :color => '#ff0000', :daches => true,  :priority => 101, },
+        'PTR'      => { :color => '#00ff00', :daches => false, :priority => 110, },
+        '!PTR'     => { :color => '#00ff00', :daches => true,  :priority => 111, },
+        'MX'       => { :color => '#0000ff', :daches => false, :priority => 120, },
+        '!MX'      => { :color => '#0000ff', :daches => true,  :priority => 121, },
+        'NS'       => { :color => '#ff00ff', :daches => false, :priority => 130, },
+        '!NS'      => { :color => '#ff00ff', :daches => true,  :priority => 131, },
+        'AAAA'     => { :color => '#990000', :daches => false, :priority => 140, },
+        '!AAAA'    => { :color => '#990000', :daches => true,  :priority => 141, },
+        'CNAME'    => { :color => '#000000', :daches => false, :priority => 200, },
+        'NXDOMAIN' => { :color => '#000000', :daches => true,  :priority => 211, },
+        'DS'       => { :color => '#000099', :daches => false, :priority => 300, },
+        '!DS'      => { :color => '#000099', :daches => true,  :priority => 301, },
+        'NSEC'     => { :color => '#009900', :daches => false, :priority => 310, },
+        '!NSEC'    => { :color => '#009900', :daches => true,  :priority => 311, },
+        'DNSKEY'   => { :color => '#009999', :daches => false, :priority => 320, },
+        '!DNSKEY'  => { :color => '#999900', :daches => true,  :priority => 321, },
+        'RRSIG'    => { :color => '#990099', :daches => false, :priority => 330, },
+        '!RRSIG'   => { :color => '#990099', :daches => true,  :priority => 331, },
+        'DLV'      => { :color => '#999999', :daches => false, :priority => 340, },
+        '!DLV'     => { :color => '#999999', :daches => true,  :priority => 341, },
+      }
+
+      DEFAULT_LINE_STYLE = {
+        :color => '#ff0000', :daches => false, :priority => 999999,
+      }
+
+      LINE_STYLE_OF.default = DEFAULT_LINE_STYLE
+
       def initialize( bindcache_datasource )
         @bindcache_datasource = bindcache_datasource
       end
@@ -38,14 +69,21 @@ module Rocuses
                                     :vertical_label => 'count per second',
                                     :rigid          => false )
 
-        @bindcache_datasource.cache.each { |type,count|
+        @bindcache_datasource.cache.sort { |a,b|
+          LINE_STYLE_OF[a[0]][:priority] <=> LINE_STYLE_OF[b[0]][:priority]
+        }.each { |record|
+          type       = record[0]
+          count      = record[1]
+          line_style = LINE_STYLE_OF[type]
+
           Utils::draw_line( graph,
                             {
                               :label  => sprintf( '%10s', type ),
                               :value  => count,
                               :factor => 1,
                               :width  => 1,
-                              :color  => '#0000ff',
+                              :color  => line_style[:color],
+                              :dashes => line_style[:dashes],
                               :format => GPRINT_FORMAT,
                             } )
         }
